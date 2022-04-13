ad72787bb12812235fc9bb5d788d3f62e8b229f8
Cian Twyford <cian.twyford@outlook.ie>
2022-04-13 09:21:37 +0100

    Pipes

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,12 +11,13 @@ export class ProductListComponent
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    listFilter: string = "cart";
     products: any[] = [
         {
           "productId": 1,
           "productName": "Leaf Rake",
           "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2021",
+          "releaseDate": "19th March 2021",
           "description": "Leaf rake with 48-inch wooden handle.",
           "price": 19.95,
           "starRating": 3.2,
@@ -26,7 +27,7 @@ export class ProductListComponent
           "productId": 2,
           "productName": "Garden Cart",
           "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2021",
+          "releaseDate": "18th March 2021",
           "description": "15 gallon capacity rolling garden cart",
           "price": 32.99,
           "starRating": 4.2,
@@ -36,7 +37,7 @@ export class ProductListComponent
           "productId": 5,
           "productName": "Hammer",
           "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2021",
+          "releaseDate": "21st May 2021",
           "description": "Curved claw steel hammer",
           "price": 8.9,
           "starRating": 4.8,
@@ -46,7 +47,7 @@ export class ProductListComponent
           "productId": 8,
           "productName": "Saw",
           "productCode": "TBX-0022",
-          "releaseDate": "May 15, 2021",
+          "releaseDate": "15th May 2021",
           "description": "15-inch steel blade hand saw",
           "price": 11.55,
           "starRating": 3.7,
@@ -56,7 +57,7 @@ export class ProductListComponent
           "productId": 10,
           "productName": "Video Game Controller",
           "productCode": "GMG-0042",
-          "releaseDate": "October 15, 2020",
+          "releaseDate": "15th October 2020",
           "description": "Standard two-button video game controller",
           "price": 35.95,
           "starRating": 4.6,
