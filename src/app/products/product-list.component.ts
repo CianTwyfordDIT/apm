7e3430845611dd1421488fe8bd8312fb42d576ca
Cian Twyford <cian.twyford@outlook.ie>
2022-04-14 16:39:51 +0100

    Service Added

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component(
 {
@@ -7,73 +9,68 @@ import { IProduct } from "./product";
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit
+export class ProductListComponent implements OnInit, OnDestroy
 {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = "cart";
-    products: IProduct[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "19th March 2021",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "18th March 2021",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "21st May 2021",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "assets/images/hammer.png"
-        },
-        {
-          "productId": 8,
-          "productName": "Saw",
-          "productCode": "TBX-0022",
-          "releaseDate": "15th May 2021",
-          "description": "15-inch steel blade hand saw",
-          "price": 11.55,
-          "starRating": 3.7,
-          "imageUrl": "assets/images/saw.png"
-        },
-        {
-          "productId": 10,
-          "productName": "Video Game Controller",
-          "productCode": "GMG-0042",
-          "releaseDate": "15th October 2020",
-          "description": "Standard two-button video game controller",
-          "price": 35.95,
-          "starRating": 4.6,
-          "imageUrl": "assets/images/xbox-controller.png"
-        }
-      ];
+    errorMessage: string = "";
+    sub!: Subscription
+    
+    private _listFilter: string = "";
+    get listFilter(): string
+    {
+      return this._listFilter;
+    }
+    set listFilter(value: string)
+    {
+      this._listFilter = value;
+      this.filteredProducts = this.performFilter(value);
+    }
 
-      ngOnInit(): void 
-      {
-        console.log("In onInit");
-      }
+    filteredProducts: IProduct[] = [];
+    products: IProduct[] = [];
+        
+    constructor(private productService: ProductService)
+    {
 
-      toggleImage(): void
+    }
+
+    ngOnInit(): void 
+    {
+      this.sub = this.productService.getProducts().subscribe(
       {
-          this.showImage = !this.showImage;
+        next: products => 
+        {
+          this.products = products
+          this.filteredProducts = this.products
+        },
+        error: err => this.errorMessage = err
       }
+      )
+
+    }
+
+    ngOnDestroy(): void 
+    {
+        this.sub.unsubscribe();
+    }
+
+    toggleImage(): void
+    {
+        this.showImage = !this.showImage;
+    }
+
+    performFilter(filterBy: string): IProduct[] 
+    {
+      filterBy = filterBy.toLowerCase();
+      return this.products.filter((product: IProduct) =>
+      product.productName.toLowerCase().includes(filterBy));
+    }
+
+    onRatingClicked(message: string): void
+    {
+      this.pageTitle = 'Product List: ' + message;
+    }
 }
